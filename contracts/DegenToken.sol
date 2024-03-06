0c59a28e768ad900b8e3aba672e4d29481953fb6
samuel1-ona <samuonanikesamuel123@gmail.com>
2024-03-06 15:07:41 +0100

    initial commit

---
+++ contracts/DegenToken.sol
@@ -0,0 +1,91 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract DegenToken {
+    address public owner;
+
+
+    uint public totalSupply;
+
+
+    mapping(address => uint) public balanceOf;
+
+
+    mapping(address => mapping(address => uint)) public allowance;
+
+
+    string public name = "Degen";
+
+
+    string public symbol = "DGN";
+
+
+    uint8 public decimals = 18;
+
+    event Transfer(address indexed from, address indexed to, uint value);
+
+
+    event Approval(address indexed owner, address indexed spender, uint value);
+
+    constructor() {
+        owner = msg.sender; // Set the contract deployer as the owner
+    }
+
+    function transfer(address recipient, uint amount) external returns (bool) {
+
+        require(balanceOf[msg.sender] >= amount, "Not enough balance");
+
+        balanceOf[msg.sender] -= amount;
+
+        balanceOf[recipient] += amount;
+
+        emit Transfer(msg.sender, recipient, amount);
+
+        return true;
+    }
+
+    function approve(address spender, uint amount) external returns (bool) {
+
+        allowance[msg.sender][spender] = amount;
+
+        emit Approval(msg.sender, spender, amount);
+
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint amount) external returns (bool) {
+        require( allowance[sender][msg.sender] >= amount, "Not enough allowance" );
+
+        allowance[sender][msg.sender] -= amount;
+
+        balanceOf[sender] -= amount;
+
+        balanceOf[recipient] += amount;
+
+        emit Transfer(sender, recipient, amount);
+
+        return true;
+    }
+
+    function mint(uint amount) external {
+
+        require(msg.sender == owner, "Only the owner can mint tokens");
+
+        balanceOf[owner] += amount;
+
+        totalSupply += amount;
+
+        emit Transfer(address(0), owner, amount);
+    }
+
+    function burn(uint amount) external {
+
+        require(balanceOf[msg.sender] >= amount, "Not enough balance to burn");
+
+        balanceOf[msg.sender] -= amount;
+
+        totalSupply -= amount;
+        
+        emit Transfer(msg.sender, address(0), amount);
+    }
+}
